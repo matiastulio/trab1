7626648086173e238a9d8b2dfecc1a2ba1eb998c
ian <ianmalm.ufrj@gmail.com>
2015-04-19 19:20:59 -0300

    Alterações

--- src/br/unb/gui/MainGui.java
+++ src/br/unb/gui/MainGui.java
@@ -36,20 +36,22 @@ public class MainGui implements Runnable {
 	private static TocaMidi tocaMidi;
 	private int volumePorCento;
 	
-	private JFrame frmMidiPlayer;
-	private JMenuItem mntmSair,mntmEscolherMidi;
-	private JMenuBar menuBar;
-	private JMenu mnArquivo;
-	private GridBagLayout gridBagLayout;
-	private JPanel painelAux,painelInfo;
-	private JSeparator separadorDados;
-	private String caminhoArquivo;
-	private JSlider sliderTempoMusica,sliderVolume;
-	private JButton btnParar,btnTocar,btnPausar;
-	private GridBagConstraints gbc_sliderTempoMusica,gbc_lblNomeArquivo,gbc_lblInicioTempo,gbc_lblBarraTime,gbc_lblFimTempo,gbc_btnParar,gbc_btnTocar,
-								gbc_btnPausar,gbc_lblVolume,gbc_sliderVolume,gbc_separadorDados,gbc_painelAux;
-	private JLabel lblNomeArquivo,lblInicioTempo,lblBarraTime,lblFimTempo,lblVolume,lblFormulaDeCompasso,lblMetro,lblAndamento,lblArmaduraDeTonalidade,
-								lblArqArmadura,lblArqCompasso,lblArqBpm,lblArqMetro;
+	private JFrame             frmMidiPlayer;
+	private JMenuItem          mntmSair,mntmEscolherMidi;
+	private JMenuBar           menuBar;
+	private JMenu              mnArquivo;
+	private GridBagLayout      gridBagLayout;
+	private JPanel             painelAux,painelInfo;
+	private JSeparator         separadorDados;
+	private String             caminhoArquivo;
+	private JSlider            sliderTempoMusica,sliderVolume;
+	private JButton            btnParar,btnTocar,btnPausar;
+	private GridBagConstraints gbc_sliderTempoMusica,gbc_lblNomeArquivo,gbc_lblInicioTempo,gbc_lblBarraTime,
+	                           gbc_lblFimTempo,gbc_btnParar,gbc_btnTocar,
+							   gbc_btnPausar,gbc_lblVolume,gbc_sliderVolume,gbc_separadorDados,gbc_painelAux;
+	private JLabel             lblNomeArquivo,lblInicioTempo,lblBarraTime,lblFimTempo,lblVolume,
+	                           lblFormulaDeCompasso,lblMetro,lblAndamento,lblArmaduraDeTonalidade,
+							   lblArqArmadura,lblArqCompasso,lblArqBpm,lblArqMetro;
 	
 
 	public static void main(String[] args) {
@@ -111,15 +113,15 @@ public class MainGui implements Runnable {
 		gbc_lblNomeArquivo = 	new GridBagConstraints();
 		
 		//labels
-		lblFimTempo =				new JLabel("23:59:59");
-		lblInicioTempo = 			new JLabel("23:59:59");
-		lblNomeArquivo = 			new JLabel("Nome do arquivo MIDI ");
-		lblBarraTime = 				new JLabel("/");
-		lblVolume = 				new JLabel("Volume");
-		lblFormulaDeCompasso = 		new JLabel("Formula de Compasso:");
-		lblMetro = 					new JLabel("Metro:");
-		lblAndamento = 				new JLabel("Andamento:");
-		lblArmaduraDeTonalidade =	new JLabel("Armadura de Tonalidade:");
+		lblFimTempo               =	new JLabel("23:59:59"               );
+		lblInicioTempo            = new JLabel("23:59:59"               );
+		lblNomeArquivo            = new JLabel("Nome do arquivo MIDI "  );
+		lblBarraTime              = new JLabel("/"                      );
+		lblVolume                 = new JLabel("Volume"                 );
+		lblFormulaDeCompasso      = new JLabel("Formula de Compasso:"   );
+		lblMetro  				  = new JLabel("Metro:"                 );
+		lblAndamento              = new JLabel("Andamento:"             );
+		lblArmaduraDeTonalidade   =	new JLabel("Armadura de Tonalidade:");
 		
 		//labels com os dados vindos dos arquivos, valores iniciais de exemplo. o Certo � eles estarem zerados ao iniciar o programa
 		lblArqCompasso = 			new JLabel("3/4");
@@ -147,36 +149,36 @@ public class MainGui implements Runnable {
 		mnArquivo.add(mntmSair);
 		
 		// configuracao do layout em gridbag, posicionamento de cada elemento do layout
-		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWidths  = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights    = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights    = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		
-		gbc_lblNomeArquivo.fill = GridBagConstraints.HORIZONTAL;
+		gbc_lblNomeArquivo.fill      = GridBagConstraints.HORIZONTAL;
 		gbc_lblNomeArquivo.gridwidth = 16;
-		gbc_lblNomeArquivo.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNomeArquivo.gridx = 0;
-		gbc_lblNomeArquivo.gridy = 1;
+		gbc_lblNomeArquivo.insets    = new Insets(0, 0, 5, 0);
+		gbc_lblNomeArquivo.gridx     = 0;
+		gbc_lblNomeArquivo.gridy     = 1;
 		
 		gbc_sliderTempoMusica.gridheight = 3;
-		gbc_sliderTempoMusica.gridwidth = 16;
-		gbc_sliderTempoMusica.fill = GridBagConstraints.HORIZONTAL;
-		gbc_sliderTempoMusica.insets = new Insets(0, 0, 5, 0);
-		gbc_sliderTempoMusica.gridx = 0;
-		gbc_sliderTempoMusica.gridy = 2;
+		gbc_sliderTempoMusica.gridwidth  = 16;
+		gbc_sliderTempoMusica.fill       = GridBagConstraints.HORIZONTAL;
+		gbc_sliderTempoMusica.insets     = new Insets(0, 0, 5, 0);
+		gbc_sliderTempoMusica.gridx      = 0;
+		gbc_sliderTempoMusica.gridy      = 2;
 		
 		gbc_lblInicioTempo.anchor = GridBagConstraints.EAST;
 		gbc_lblInicioTempo.insets = new Insets(0, 0, 5, 5);
-		gbc_lblInicioTempo.gridx = 1;
-		gbc_lblInicioTempo.gridy = 5;
+		gbc_lblInicioTempo.gridx  = 1;
+		gbc_lblInicioTempo.gridy  = 5;
 		
 		gbc_lblBarraTime.insets = new Insets(0, 0, 5, 5);
 		gbc_lblBarraTime.gridx = 2;
 		gbc_lblBarraTime.gridy = 5;
 		
 		gbc_lblFimTempo.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFimTempo.gridx = 3;
-		gbc_lblFimTempo.gridy = 5;
+		gbc_lblFimTempo.gridx  = 3;
+		gbc_lblFimTempo.gridy  = 5;
 	
 		btnParar.setFont(UIManager.getFont("TextArea.font"));
 		gbc_btnParar.fill = GridBagConstraints.HORIZONTAL;
@@ -191,51 +193,52 @@ public class MainGui implements Runnable {
 		
 		btnPausar.setFont(UIManager.getFont("TextArea.font"));
 		gbc_btnPausar.insets = new Insets(0, 0, 5, 5);
-		gbc_btnPausar.gridx = 8;
-		gbc_btnPausar.gridy = 5;
+		gbc_btnPausar.gridx  = 8;
+		gbc_btnPausar.gridy  = 5;
 		
 		gbc_lblVolume.insets = new Insets(0, 0, 5, 5);
-		gbc_lblVolume.fill = GridBagConstraints.HORIZONTAL;
-		gbc_lblVolume.gridx = 11;
-		gbc_lblVolume.gridy = 5;
+		gbc_lblVolume.fill   = GridBagConstraints.HORIZONTAL;
+		gbc_lblVolume.gridx  = 11;
+		gbc_lblVolume.gridy  = 5;
 		
 		gbc_sliderVolume.gridwidth = 4;
-		gbc_sliderVolume.insets = new Insets(0, 0, 5, 0);
-		gbc_sliderVolume.gridx = 12;
-		gbc_sliderVolume.gridy = 5;
+		gbc_sliderVolume.insets    = new Insets(0, 0, 5, 0);
+		gbc_sliderVolume.gridx     = 12;
+		gbc_sliderVolume.gridy     = 5;
 		
 		separadorDados.setForeground(Color.WHITE);
 		separadorDados.setBackground(Color.BLACK);
 		gbc_separadorDados.gridwidth = 16;
-		gbc_separadorDados.fill = GridBagConstraints.HORIZONTAL;
-		gbc_separadorDados.insets = new Insets(0, 0, 5, 0);
-		gbc_separadorDados.gridx = 0;
-		gbc_separadorDados.gridy = 7;
+		gbc_separadorDados.fill      = GridBagConstraints.HORIZONTAL;
+		gbc_separadorDados.insets    = new Insets(0, 0, 5, 0);
+		gbc_separadorDados.gridx     = 0;
+		gbc_separadorDados.gridy     = 7;
 		
 		
 		painelAux.setBackground(Color.WHITE);
 		painelAux.setLayout(null);
-		gbc_painelAux.fill = GridBagConstraints.BOTH;
+		gbc_painelAux.fill       = GridBagConstraints.BOTH;
 		gbc_painelAux.gridheight = 6;
-		gbc_painelAux.gridwidth = 16;
-		gbc_painelAux.gridx = 0;
-		gbc_painelAux.gridy = 8;
+		gbc_painelAux.gridwidth  = 16;
+		gbc_painelAux.gridx      = 0;
+		gbc_painelAux.gridy      = 8;
 		
 
 		//adiciona tudo que foi criado e configurado ao frame
 		frmMidiPlayer.getContentPane().setLayout(gridBagLayout);
-		frmMidiPlayer.getContentPane().add(painelAux, gbc_painelAux);
-		frmMidiPlayer.getContentPane().add(separadorDados, gbc_separadorDados);
-		frmMidiPlayer.getContentPane().add(sliderVolume, gbc_sliderVolume);
-		frmMidiPlayer.getContentPane().add(lblVolume, gbc_lblVolume);
-		frmMidiPlayer.getContentPane().add(btnPausar, gbc_btnPausar);
-		frmMidiPlayer.getContentPane().add(btnTocar, gbc_btnTocar);
-		frmMidiPlayer.getContentPane().add(btnParar, gbc_btnParar);
-		frmMidiPlayer.getContentPane().add(lblFimTempo, gbc_lblFimTempo);
-		frmMidiPlayer.getContentPane().add(lblBarraTime, gbc_lblBarraTime);
-		frmMidiPlayer.getContentPane().add(lblInicioTempo, gbc_lblInicioTempo);
+		
+		frmMidiPlayer.getContentPane().add(painelAux        , gbc_painelAux        );
+		frmMidiPlayer.getContentPane().add(separadorDados   , gbc_separadorDados   );
+		frmMidiPlayer.getContentPane().add(sliderVolume     , gbc_sliderVolume     );
+		frmMidiPlayer.getContentPane().add(lblVolume        , gbc_lblVolume        );
+		frmMidiPlayer.getContentPane().add(btnPausar        , gbc_btnPausar        );
+		frmMidiPlayer.getContentPane().add(btnTocar         , gbc_btnTocar         );
+		frmMidiPlayer.getContentPane().add(btnParar         , gbc_btnParar         );
+		frmMidiPlayer.getContentPane().add(lblFimTempo      , gbc_lblFimTempo      );
+		frmMidiPlayer.getContentPane().add(lblBarraTime     , gbc_lblBarraTime     );
+		frmMidiPlayer.getContentPane().add(lblInicioTempo   , gbc_lblInicioTempo   );
 		frmMidiPlayer.getContentPane().add(sliderTempoMusica, gbc_sliderTempoMusica);
-		frmMidiPlayer.getContentPane().add(lblNomeArquivo, gbc_lblNomeArquivo);
+		frmMidiPlayer.getContentPane().add(lblNomeArquivo   , gbc_lblNomeArquivo   );
 		
 		//********config do painel de informacao********//
 		painelInfo.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255), new Color(0, 0, 255)));
@@ -343,27 +346,27 @@ public class MainGui implements Runnable {
 		  
 		  JFileChooser arquivo = new JFileChooser(".");
 
-		  UIManager.put("FileChooser.openDialogTitleText", "Abrir");
-		  UIManager.put("FileChooser.lookInLabelText", "Consultar em");
-		  UIManager.put("FileChooser.openButtonText", "Abrir");
-		  UIManager.put("FileChooser.cancelButtonText", "Cancelar");
-		  UIManager.put("FileChooser.fileNameLabelText", "Nome do Arquivo");
-		  UIManager.put("FileChooser.filesOfTypeLabelText", "Tipo de Arquivo");
-		  UIManager.put("FileChooser.openButtonToolTipText", "Abrir o Arquivo Selecionado");
-		  UIManager.put("FileChooser.cancelButtonToolTipText","Cancelar");
-		  UIManager.put("FileChooser.fileNameHeaderText","Nome");
-		  UIManager.put("FileChooser.upFolderToolTipText", "Subir um Nivel");
-		  UIManager.put("FileChooser.homeFolderToolTipText","Area de Trabalho");
-		  UIManager.put("FileChooser.newFolderToolTipText","Criar Nova Pasta");
-		  UIManager.put("FileChooser.listViewButtonToolTipText","Lista");
-		  UIManager.put("FileChooser.newFolderButtonText","Criar Nova Pasta");
-		  UIManager.put("FileChooser.renameFileButtonText", "Renomear");
-		  UIManager.put("FileChooser.deleteFileButtonText", "Deletar");
-		  UIManager.put("FileChooser.filterLabelText", "Tipo");
-		  UIManager.put("FileChooser.detailsViewButtonToolTipText", "Detalhes");
-		  UIManager.put("FileChooser.fileSizeHeaderText","Tamanho");
-		  UIManager.put("FileChooser.fileDateHeaderText", "Data de Modificacao");
-		  UIManager.put("FileChooser.acceptAllFileFilterText", "Todos os Arquivos");
+		  UIManager.put("FileChooser.openDialogTitleText"          , "Abrir"                      );
+		  UIManager.put("FileChooser.lookInLabelText"              , "Consultar em"               );
+		  UIManager.put("FileChooser.openButtonText"               , "Abrir"                      );
+		  UIManager.put("FileChooser.cancelButtonText"             , "Cancelar"                   );                 
+		  UIManager.put("FileChooser.fileNameLabelText"            , "Nome do Arquivo"            );
+		  UIManager.put("FileChooser.filesOfTypeLabelText"         , "Tipo de Arquivo"            );
+		  UIManager.put("FileChooser.openButtonToolTipText"        , "Abrir o Arquivo Selecionado");
+		  UIManager.put("FileChooser.cancelButtonToolTipText"      , "Cancelar"                   );
+		  UIManager.put("FileChooser.fileNameHeaderText"           , "Nome"                       );
+		  UIManager.put("FileChooser.upFolderToolTipText"          , "Subir um Nivel"             );
+		  UIManager.put("FileChooser.homeFolderToolTipText"        ,"Area de Trabalho"            );
+		  UIManager.put("FileChooser.newFolderToolTipText"         ,"Criar Nova Pasta"            );
+		  UIManager.put("FileChooser.listViewButtonToolTipText"    ,"Lista"                       );
+		  UIManager.put("FileChooser.newFolderButtonText"          ,"Criar Nova Pasta"            );
+		  UIManager.put("FileChooser.renameFileButtonText"         , "Renomear"                   );
+		  UIManager.put("FileChooser.deleteFileButtonText"         , "Deletar"                    );
+		  UIManager.put("FileChooser.filterLabelText"              , "Tipo"                       );
+		  UIManager.put("FileChooser.detailsViewButtonToolTipText" , "Detalhes"                   );
+		  UIManager.put("FileChooser.fileSizeHeaderText"           ,"Tamanho"                     );
+		  UIManager.put("FileChooser.fileDateHeaderText"           , "Data de Modificacao"        );
+		  UIManager.put("FileChooser.acceptAllFileFilterText"      , "Todos os Arquivos"          );
 		  
 		  SwingUtilities.updateComponentTreeUI(arquivo);
 
